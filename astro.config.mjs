00a3a63180e74e3b136829ad8ec008c4461a0b1f
Karl Aase Blanco <karl23aase@gmail.com>
2024-05-28 11:31:05 -0600

    Analytics Implementation
    Minor Release 1.0.7

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,22 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import astroI18next from "astro-i18next";
 import react from "@astrojs/react";
+import partytown from "@astrojs/partytown";
 
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://kaserdevsec-portafolio.netlify.app/",
-  integrations: [tailwind(), astroI18next(), react(), sitemap()],
+  integrations: [
+    tailwind(),
+    astroI18next(),
+    react(),
+    sitemap(),
+    partytown({
+      config: {
+        forward: ["dataLayer.push"],
+      },
+    }),
+  ],
 });
