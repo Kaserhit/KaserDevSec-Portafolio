88fcfbbfba5a69571b3b3947c7f2a32476196b4f
Karl Aase Blanco <karl23aase@gmail.com>
2024-05-28 11:08:13 -0600

    sitemap integration
    Minor Release 1.0.6

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,10 @@ import tailwind from "@astrojs/tailwind";
 import astroI18next from "astro-i18next";
 import react from "@astrojs/react";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), astroI18next(), react()],
+  site: "https://kaserdevsec-portafolio.netlify.app/",
+  integrations: [tailwind(), astroI18next(), react(), sitemap()],
 });
